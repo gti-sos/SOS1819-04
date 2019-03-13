27c44128208e62cc6739a1dd4b760de58e98faf3
sos1819amma <ana.marin.medina98@gmail.com>
2019-03-13 10:06:30 +0000

    new index.js

--- SOS1819-04/index.js
+++ SOS1819-04/index.js
@@ -1,16 +1,18 @@
 var express = require("express");
 var app = express();
-
-var moment = require("moment");
 var port = process.env.PORT || 8080;
 
-app.use("/", express.static("/home/ubuntu/workspace/Servidor_Web/public"));
+app.use("/",
+    //dir name es la carpeta donde se esta ejecutando node
+    express.static(__dirname+"/public"));
 
-app.get("/time", (request, response) => 
-            {
-                var hora = moment().format('MMMM Do YYYY, h:mm:ss a');
-                response.send(hora);
-            }
-        );
 
-app.listen(port, () => {console.log("Server Ready and Listen in port", port);});
+app.get("/time",(request,respone)=>{
+    respone.send(new Date());
+});
+
+
+app.listen(port, () =>
+{
+    console.log("Magic is happening in port "+port)
+});
