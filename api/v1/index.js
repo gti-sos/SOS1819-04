9ecce0f7bcde5ba0ade8678d81fa9b396780c103
sos1819amma <ana.marin.medina98@gmail.com>
2019-04-02 08:22:56 +0000

    happiness-stats

--- api/v1/index.js
+++ api/v1/index.js
@@ -5,4 +5,9 @@ const suicide_rates = require("./suicide-rates");
 
 router.use("/suicide-rates", suicide_rates);
 
+//para happiness-stats
+const happiness_stats = require("./happiness-stats");
+router.use("/happiness-stats", happiness_stats);
+
+
 module.exports = router;
