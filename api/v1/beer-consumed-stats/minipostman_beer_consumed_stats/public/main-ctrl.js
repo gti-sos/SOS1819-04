82f3ecc597f96b25bea3093f1582a18bcd7318c5
sos1819jpcc <sevillanocarmona@gmail.com>
2019-04-10 09:26:20 +0000

    update jp

--- api/v1/beer-consumed-stats/minipostman_beer_consumed_stats/public/main-ctrl.js
+++ api/v1/beer-consumed-stats/minipostman_beer_consumed_stats/public/main-ctrl.js
@@ -7,7 +7,7 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
     console.log("MainCtrl initialized!");
     $scope.url = "/api/v1/beer-consumed-stats";
 
-    $scope.send = function() {
+    $scope.get = function() {
         $http.get($scope.url).then(function(response) {
             $scope.data = JSON.stringify(response.data, null, 2);
         });
