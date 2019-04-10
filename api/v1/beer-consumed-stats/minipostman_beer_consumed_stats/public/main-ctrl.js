5af863159b0f020cf9c3f5f48d272eeb999cd578
sos1819jpcc <sevillanocarmona@gmail.com>
2019-04-10 08:29:14 +0000

    update jp

--- api/v1/beer-consumed-stats/minipostman_beer_consumed_stats/public/main-ctrl.js
+++ api/v1/beer-consumed-stats/minipostman_beer_consumed_stats/public/main-ctrl.js
@@ -1,20 +1,20 @@
 /* global angular $scope*/
-        
-    var app = angular.module("MiniPostmanApp");
-    console.log("MiniPostmanApp initialized!");
-            
-    app.controller("MainCtrl", ["$scope","$http", function ($scope,$http){
-        console.log("MainCtrl initialized!");
-        $scope.url = "/api/v1/beer-consumed-stats";
-                
-        $scope.send = function (){
-            $http.get($scope.url).then(function (response){
-                $scope.data = JSON.stringify(response.data,null,2);
-            });
-        }
-         $scope.loadInitial = function (){
-            $http.get($scope.url + "/loadInitialData").then(function (response){
-                $scope.data = JSON.stringify(response.data,null,2);
-            });
-        }
-    }]);
+
+var app = angular.module("MiniPostmanApp");
+console.log("MiniPostmanApp initialized!");
+
+app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
+    console.log("MainCtrl initialized!");
+    $scope.url = "/api/v1/beer-consumed-stats";
+
+    $scope.send = function() {
+        $http.get($scope.url).then(function(response) {
+            $scope.data = JSON.stringify(response.data, null, 2);
+        });
+    }
+    $scope.loadInitial = function() {
+        $http.get($scope.url + "/loadInitialData").then(function(response) {
+            $scope.data = JSON.stringify(response.data, null, 2);
+        });
+    }
+}]);
