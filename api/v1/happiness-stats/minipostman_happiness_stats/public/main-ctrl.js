5274242c915f9b266e337493c741ed957fe97034
sos1819amma <ana.marin.medina98@gmail.com>
2019-04-24 10:02:10 +0000

    a

--- api/v1/happiness-stats/minipostman_happiness_stats/public/main-ctrl.js
+++ api/v1/happiness-stats/minipostman_happiness_stats/public/main-ctrl.js
@@ -1,5 +1,5 @@
 
-/* global angular $scope*/
+/* global angular */
 
 var app = angular.module("MiniPostmanApp");
 
@@ -9,64 +9,17 @@ app.controller("MainCtrl", ["$scope","$http", function ($scope, $http){
                     $scope.url = "/api/v1/happiness-stats/";
                     console.log("out get");
                     
-        $scope.send = function() {
-            console.log("in get");
-            
-            $http.get($scope.url).then(function(response) {
-                $scope.status = response.status;
-                $scope.data = JSON.stringify(response.data, null, 2);
-            }, function(error) {
-                $scope.status = error.status;
-                $scope.data = "";
-            });
-        };
-            
-        /*
-        }
-        $scope.loadInitial = function() {
-            $http.get($scope.url + "./loadInitialData").then(function(response) {
-                $scope.data = JSON.stringify(response.data, null, 2);
-            });
-        }
-        $scope.delete = function() {
-            $http.delete($scope.url).then(function(response) {
-                $scope.data = JSON.stringify(response.data, null, 2);
-            });
-        }
-        */
-        /*
-        $scope.post = function() {
-            $http.post($scope.url, {
-                country: $scope.country,
-                year: parseInt($scope.year),
-                happinessScore: parseFloat($scope.happinessScore),
-                lowerLimitTrust: parseFloat($scope.lowerLimitTrust),
-                upperLimitTrust: parseFloat($scope.upperLimitTrust)
-            }).then(function(response) {
-                console.log("exito");
-                $scope.status = response.status;
-            }, function(error) {
-                console.log("fallo");
-                $scope.status = error.status;
-            });
-           
-        };
-        
-
-        $scope.put = function() {
-            $http.put($scope.url, {
-                country: $scope.country,
-                year: parseInt($scope.year),
-                happinessScore: parseFloat($scope.happinessScore),
-                lowerLimitTrust: parseFloat($scope.lowerLimitTrust),
-                upperLimitTrust: parseFloat($scope.upperLimitTrust)
-            }).then(function(response) {
-                $scope.status = response.status;
-            }, function(error) {
-                $scope.status = error.status;
-            });
-        };*/
-        
+        $scope.send = function(){
+                $http.get($scope.url).then(function(response){
+                    $scope.status = response.status;
+                    $scope.data = JSON.stringify(response.data,null,2);
+                }, function (error){
+                    $scope.status = error.status;
+                    $scope.data = "";
+                    });
+                };
+                
+                
         $scope.post = function() {
             $http.post($scope.url,$scope.data 
             ).then(function(response) {
