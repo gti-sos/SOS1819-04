c95b85a6765f9d41bd2bd216b0da0f4ecae67b8d
sos1819afg <adrianfergo93@gmail.com>
2019-05-13 22:54:33 +0000

    Fix ui structure.

--- ui/v1/beer-consumed-stats/public/beer_consumed_stats_frontend.js
+++
@@ -1,4 +0,0 @@
-/*global angular*/
-
-angular.module("BeerConsumedStatsFrontEnd", []);
-console.log("Front End [Beer-consumed-stats] initialized!");
--- ui/v1/suicide-rates/public/suicide_rates_frontend.js
+++
@@ -1,5 +0,0 @@
-/* global angular */
-
-angular.module("SuicideRatesFrontEnd", []);
-
-console.log("[Suicide Rates] Suicide Rates Front-End is initialized and Ready to Use !!.");
