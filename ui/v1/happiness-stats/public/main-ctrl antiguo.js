942df371568b8a8f9e0d0d58b6fa8813da098af8
sos1819jpcc <sevillanocarmona@gmail.com>
2019-05-13 14:39:59 +0000

    busqueda

--- ui/v1/happiness-stats/public/main-ctrl antiguo.js
+++ ui/v1/happiness-stats/public/main-ctrl antiguo.js
@@ -1,30 +1,34 @@
 /*global angular*/
 
 angular
-        .module("BeerConsumedStatsFrontEnd")
+        .module("HappinessApp")
         .controller("MainCtrl",["$scope","$http", function ($scope,$http){
-            console.log("[Beer] Main Controller initialized!");
-            var API = "/api/v1/beer-consumed-stats";
-            refresh();
+            console.log("Main Controller initialized.");
+            var API = "/api/v1/happiness-stats";
             
-            function refresh(){
+            refresh(API);
+            
+            function refresh(API){
                 
                 $http.get(API).then(function(response) {
                         
                         console.log("GET Request revived to " + "<" + API + ">");
                         console.log("Data Received: " + JSON.stringify(response.data, null,2));
-                        $scope.beer_consumeds = response.data;
+                        $scope.happiness_stats = response.data;
+                        
                     }
                 );
                 
             }
             
-            $scope.postData = function(){
-                    var newBeer = $scope.newBeer;
-                    console.log("Nuevo Recurso de Cerveza");
-                    $http.post(API ,newBeer).then(function(response){
+            $scope.addHappiness = function(){
+                    var newHappiness = $scope.newHappiness;
+                    newHappiness.year = parseInt($scope.newHappiness.year);
+                    console.log(newHappiness);
+                    console.log("Nuevo Recurso de felicidad");
+                    $http.post(API ,newHappiness).then(function(response){
                         console.log("POST Response: "+ response.status +" "+ response.data);
-                        refresh();
+                        refresh(API);
                         $scope.status = "Recurso creado";
                     }, function (error){
                         $scope.status = error.status;
@@ -35,7 +39,7 @@ angular
                             $scope.status = $scope.status + " - El recurso ya existe";
                     }
                         if($scope.status == 400){
-                            $scope.status = $scope.status + " - El recurso ya existe";
+                            $scope.status = $scope.status + " - No se pueden quedar campos en blanco";
                     }
                         $scope.data = "";
                         });
@@ -54,10 +58,10 @@ angular
             $scope.busqueda = function(country, year){
                 var search = "/?";
                 if ($scope.consulta.country) {
-                    search += ("&country=" + $scope.consulta.country);
+                    search += "country=" + $scope.consulta.country + "&";
                 }
                 if ($scope.consulta.year) {
-                    search += ("&year=" + $scope.consulta.year);
+                    search += "year=" + $scope.consulta.year;
                 }
                 refresh(API + search);
                 search = "/?";
@@ -67,7 +71,7 @@ angular
             $scope.deleteAll = function() {
                 $http.delete(API).then(function(response) {
                     console.log("Response : " + response.status + response.data);
-                    refresh();
+                    refresh(API);
                     $scope.status = "Recursos eliminados";
                 }, function(error) {
                     $scope.status = error.status;
@@ -79,7 +83,7 @@ angular
                 $http.delete(API +"/" +country + "/"+year).then(function(response) {
                     console.log("Borrando: " + country + " - " + year);
                     console.log("Response : " + response.status + response.data);
-                    refresh();
+                    refresh(API);
                     $scope.status = country +"ha sido eliminado";
                 }, function(error) {
                     $scope.status = error.status;
@@ -94,7 +98,7 @@ angular
                 $http.get(API + "/loadInitialData").then(function(response) {
                 console.log("Respuesta : " + response.status + response.data);
                 $scope.status = "Restauracion realizada con exito";
-                refresh();
+                refresh(API);
             }).catch(function(response) {
                 $scope.status = response.status;
                 if($scope.status == 409){
@@ -106,3 +110,4 @@ angular
               
             
         }]);
+
