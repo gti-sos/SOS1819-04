4a3107e8de2706a5d9c5e3b137bd35cedc1385a4
sos1819jpcc <sevillanocarmona@gmail.com>
2019-05-15 09:14:41 +0000

    commit for test beer

--- tests_beer_consumed_stats/e2e/TCBCS02-create.js
+++ tests_beer_consumed_stats/e2e/TCBCS02-create.js
@@ -3,18 +3,21 @@ describe("Check if a new data beer is loaded: ", function() {
 
     it("should show a bunch of data", function() {
         browser.get("https://sos1819-04.herokuapp.com/#!/ui/v1/beer-consumed-stats")
+        //browser.get("https://sos1819-04-sos1819jpcc.c9users.io/#!/ui/v1/beer-consumed-stats")
             .then(function() {
-                element.all(by.repeater('beer_stat in beer_stats')).then(function(initialStats) {
+                element.all(by.repeater('beer_consumed in beer_consumeds')).then(function(initialStats) {
 
-                    element(by.model('initBeerStat.country')).sendKeys('testCountry');
-                    element(by.model('initBeerStat.year')).sendKeys(1);
-                    element(by.model('initBeerStat.rating')).sendKeys(1.1);
-                    element(by.model('initBeerStat.variation')).sendKeys(1.1);
-                    element(by.model('initBeerStat.countryConsumition')).sendKeys(1);
+                    element(by.model('newBeer.country')).sendKeys('testCountry');
+                    element(by.model('newBeer.year')).sendKeys(1);
+                    element(by.model('newBeer.rating')).sendKeys(1.1);
+                    element(by.model('newBeer.variation')).sendKeys(1.1);
+                    element(by.model('newBeer.countryConsumition')).sendKeys(1);
 
-                    element(by.buttonText('Add')).click();
+                    //element(by.buttonText('Add')).click();
+                    element(by.css('[value="Añadir"]')).click();
+                                    
                     element
-                        .all(by.repeater("beer_stat in beer_stats"))
+                        .all(by.repeater("beer_consumed in beer_consumeds"))
                         .then(function(finalBeer) {
 
                             expect(finalBeer.length).toEqual(initialStats.length + 1);
--- tests_beer_consumed_stats/e2e/TCBCS03-delete.js
+++ tests_beer_consumed_stats/e2e/TCBCS03-delete.js
@@ -3,11 +3,12 @@ describe("Check if a new data beer is loaded: ", function() {
 
     it("should show a bunch of data", function() {
         browser.get("https://sos1819-04.herokuapp.com/#!/ui/v1/beer-consumed-stats")
+        //browser.get("https://sos1819-04-sos1819jpcc.c9users.io/#!/ui/v1/beer-consumed-stats")
             .then(function() {
-                element.all(by.repeater('beer_stat in beer_stats')).then(function(initialBeer) {
+                element.all(by.repeater('beer_consumed in beer_consumeds')).then(function(initialBeer) {
                     element.all(by.id("borrar1")).last().click();
 
-                    element.all(by.repeater("beer_stat in beer_stats"))
+                    element.all(by.repeater("beer_consumed in beer_consumeds"))
                         .then(function(finalBeer) {
                             expect(finalBeer.length).toEqual(initialBeer.length - 1);
                         });
